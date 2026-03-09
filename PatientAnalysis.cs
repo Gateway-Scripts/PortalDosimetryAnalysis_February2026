392dc4506302f5844b68e3fc0cc4a6633e74dcc0
mschmidt <mschmidt@Master-AE>
2026-03-09 21:24:24 +0000

    Add project files.

---
+++ PatientAnalysis.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using VMS.CA.Scripting;
+
+namespace VMS.DV.PD.Scripting
+{
+
+    public class Script
+    {
+
+        public Script()
+        {
+        }
+
+        public void Execute(ScriptContext context, System.Windows.Window window)
+        {
+            // TODO : Add here your code that is called when the script is launched from Portal Dosimetry
+        }
+
+    }
+
+}
